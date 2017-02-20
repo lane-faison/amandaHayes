6ffe333024831e27bff9443ff9f2600c316b6102
lanefaison2 <faisonusmc@gmail.com>
2017-02-20 01:50:35 -0700

    Table Top API added

---
+++ public/assets/scripts/index.js
@@ -0,0 +1,14 @@
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/10YvhPr0LEZr0-GBWsVcOJ5YTUslVCmaQBHbVy9kmVhk/pubhtml';
+
+function init() {
+  Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+}
+
+function showInfo(data, tabletop) {
+  alert('Successfully processed!')
+  console.log(data);
+}
+
+window.addEventListener('DOMContentLoaded', init)
